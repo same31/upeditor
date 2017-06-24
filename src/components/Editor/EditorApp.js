76fe898ce6093ebc5fd802762dc01a6860b9dc48
Mael Jarnole <mael.jarnole@gmail.com>
2017-06-24 11:37:52 +0200

    Added material ui and EditorComponent

--- src/components/Editor/EditorApp.js
+++ src/components/Editor/EditorApp.js
@@ -1,13 +1,19 @@
 import React, {Component} from 'react';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import EditorComponent from './components/EditorComponent';
 
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 import './style.css';
 
 class EditorApp extends Component {
     render () {
         return (
-            <div className="EditorApp">
-                Editor
-            </div>
+            <MuiThemeProvider>
+                <EditorComponent className="Editor"/>
+            </MuiThemeProvider>
         );
     }
 }
---
+++ src/components/Editor/components/EditorComponent.js
@@ -0,0 +1,11 @@
+/**
+ * Created by mael on 24/06/17.
+ */
+import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+
+const EditorComponent = () => (
+    <RaisedButton label="Default" />
+);
+
+export default EditorComponent;
