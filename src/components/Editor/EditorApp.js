c7a0c3097abd50e25f83a841b5c9a11930841d3f
same31 <matthieu.mitrani@gmail.com>
2017-06-24 11:43:02 +0200

    jquery added

--- src/components/Editor/EditorApp.js
+++ src/components/Editor/EditorApp.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import $ from 'jquery';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import EditorComponent from './components/EditorComponent';
