d6806a328727925e3d620ffe4aa30460fb1b69f8
same31 <matthieu.mitrani@gmail.com>
2017-06-24 22:42:04 +0200

    linter

--- src/components/Editor/components/EditorComponent.js
+++ src/components/Editor/components/EditorComponent.js
@@ -2,7 +2,6 @@
  * Created by mael on 24/06/17.
  */
 import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
 import {
     Drawer, AppBar, List, ListItem, Paper,
     Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle, Menu,
@@ -14,12 +13,10 @@ import Title from 'material-ui/svg-icons/editor/title';
 import InsertPhoto from 'material-ui/svg-icons/editor/insert-photo';
 import FormatListBulleted from 'material-ui/svg-icons/editor/format-list-bulleted';
 import FormatListNumbered from 'material-ui/svg-icons/editor/format-list-numbered';
-import Intel from '../Internalisation';
 import ActionLanguage from 'material-ui/svg-icons/action/language';
-import {parseHTML, getHTMLSemanticErrorList} from '../htmlUtils';
+import {getHTMLSemanticErrorList} from '../htmlUtils';
 import htmlOKContent from '../input/Example_01_deux_colonnes.html';
 import Internalisation from '../Internalisation.js';
-
 
 const documentStyle = {
     margin:    20,
@@ -33,67 +30,71 @@ export default class EditorComponent extends Component {
         super(props);
         this.htmlInput = [];
         this.state     = {
-            open:         false,
-            content:      { __html: "<div class='info-div" +
-            "' contenteditable='false'>" +
-            "No document, select a document to edit in the main menu.</div>" },
-            title:        "No document",
-            openFileMenu: false,
-            languageMenu: false,
+            open:          false,
+            content:       {
+                __html: "<div class='info-div" +
+                        "' contenteditable='false'>" +
+                        "No document, select a document to edit in the main menu.</div>"
+            },
+            title:         "No document",
+            openFileMenu:  false,
+            languageMenu:  false,
             errorSelected: false,
-            errorMessage: "",
-            language : 'en-EN',
-            intel : null
+            errorMessage:  "",
+            language:      'en-EN',
+            intel:         null
         };
     }
 
     clickHandler = (e) => {
         // Getting an array of DOM elements
         // Then finding which element was clicked
-        const nodes = Array.prototype.slice.call( e.currentTarget.children );
-        let index = e.target;
-        const errorMessage = index.getAttribute("data-error-message");
-        if(errorMessage) {
+        const index        = e.target,
+              errorMessage = index.getAttribute("data-error-message");
+
+        if (errorMessage) {
             this.setState({
                 errorSelected: true,
-                errorMessage: errorMessage
+                errorMessage:  errorMessage
             });
         } else {
             this.setState({
                 errorSelected: false,
-                errorMessage: ""
+                errorMessage:  ""
             });
         }
     };
 
-
-
-    componentWillMount() {
+    componentWillMount () {
         this.setLanguage(this.state.language);
     }
 
     setLanguage (language) {
         const intel = Internalisation(language);
         this.setState({
-            intel: intel,
-            content: { __html: "<div class='info-div'" +
-            " contenteditable='false'>" +
-                intel.getMsg("noDocumentLong") + "</div>" },
-            title: intel.getMsg("noDocument")
-        }, () => {this.htmlInput = [
-            {
-                title:   "HTML OK",
-                content: { __html: htmlOKContent }
+            intel:   intel,
+            content: {
+                __html: "<div class='info-div'" +
+                        " contenteditable='false'>" +
+                        intel.getMsg("noDocumentLong") + "</div>"
             },
-            {
-                title:   "HTML KO",
-                content: { __html: "<h2>HELLO WORLD</h2><p>A stepping stone to the ultimate success</p><div>Say hello to my little DIV</div>" }
-            },
-            {
-                title:   intel.getMsg("emptyDocument"),
-                content: { __html: "" }
-            }
-        ];});
+            title:   intel.getMsg("noDocument")
+        }, () => {
+            this.htmlInput = [
+                {
+                    title:   "HTML OK",
+                    content: { __html: htmlOKContent }
+                },
+                {
+                    title:   "HTML KO",
+                    content: { __html: "<h2>HELLO WORLD</h2><p>A stepping stone to the ultimate success</p><div>Say hello to my little DIV</div>" }
+                },
+                {
+                    title:   intel.getMsg("emptyDocument"),
+                    content: { __html: "" }
+                }
+            ];
+        });
     }
 
     toggleDrawer () {
@@ -162,8 +163,9 @@ export default class EditorComponent extends Component {
             this.toggleDrawer();
         });
     }
+
     modifyLanguage (newLanguage) {
-        if(this.state.intel.locales[newLanguage] !== undefined){
+        if (this.state.intel.locales[newLanguage] !== undefined) {
             this.setLanguage(newLanguage);
             this.handleLanguageMenuClose();
         }
@@ -174,12 +176,12 @@ export default class EditorComponent extends Component {
     }
 
     onFileSelected (event) {
-        var tgt = event.target || window.event.srcElement,
+        var tgt   = event.target || window.event.srcElement,
             files = tgt.files;
 
         // FileReader support
         if (FileReader && files && files.length) {
-            var fr = new FileReader();
+            var fr    = new FileReader();
             fr.onload = function () {
 
                 //TODO it will be in an other div to put this img, but the BASE64 is in fr.result
@@ -192,7 +194,7 @@ export default class EditorComponent extends Component {
         else {
             this.setState({
                 errorSelected: true,
-                errorMessage: this.state.intel.getMsg("functionalityNotWorking")
+                errorMessage:  this.state.intel.getMsg("functionalityNotWorking")
             });
             // fallback -- perhaps submit the input to an iframe and temporarily store
             // them on the server until the user's session ends.
@@ -210,13 +212,16 @@ export default class EditorComponent extends Component {
                     onRequestChange={(open) => this.setState({ open })}>
                     <List>
                         <Subheader>{getMsg("titreSelectDocument")}</Subheader>
-                        {this.htmlInput.map(listItem => <ListItem key={"htmlInput."+listItem.title} primaryText={listItem.title} leftIcon={<InsertDriveFile />} onClick={this.editDocument.bind(this, listItem)} />)}
+                        {this.htmlInput.map(listItem => <ListItem key={"htmlInput." + listItem.title} primaryText={listItem.title}
+                                                                  leftIcon={<InsertDriveFile />}
+                                                                  onClick={this.editDocument.bind(this, listItem)}/>)}
                     </List>
                 </Drawer>
 
-                <AppBar   title="UpEditor"
-                          onLeftIconButtonTouchTap={this.toggleDrawer.bind(this)} iconElementRight={<IconButton><ActionLanguage /></IconButton>}
-                          onRightIconButtonTouchTap={this.openLanguageMenu}/>
+                <AppBar title="UpEditor"
+                        onLeftIconButtonTouchTap={this.toggleDrawer.bind(this)}
+                        iconElementRight={<IconButton><ActionLanguage /></IconButton>}
+                        onRightIconButtonTouchTap={this.openLanguageMenu}/>
                 <Popover
                     open={this.state.languageMenu}
                     anchorEl={this.state.anchorEl}
@@ -235,20 +240,20 @@ export default class EditorComponent extends Component {
                         <ToolbarGroup firstChild={true}>
                             <IconMenu
                                 iconButtonElement={<IconButton><Title /></IconButton>}
-                                anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
-                                targetOrigin={{horizontal: 'left', vertical: 'bottom'}}>
-                                <MenuItem primaryText={<h1>Title 1</h1>} />
-                                <MenuItem primaryText={<h2>Title 2</h2>} />
-                                <MenuItem primaryText={<h3>Title 3</h3>} />
+                                anchorOrigin={{ horizontal: 'left', vertical: 'bottom' }}
+                                targetOrigin={{ horizontal: 'left', vertical: 'bottom' }}>
+                                <MenuItem primaryText={<h1>Title 1</h1>}/>
+                                <MenuItem primaryText={<h2>Title 2</h2>}/>
+                                <MenuItem primaryText={<h3>Title 3</h3>}/>
                             </IconMenu>
                             <IconButton><FormatListNumbered/></IconButton>
                             <IconButton><FormatListBulleted/></IconButton>
                             <ToolbarSeparator/>
-                            <IconButton onClick={this.clickInsertPhoto} ><InsertPhoto />
-                                <input id="chooseFileModal" style="visibility: hidden" type="file" accept="image/*" onChange={this.onFileSelected} />
-                                { /*TO DELETE THIS LINE IMG, ONLY FOR TEST*/}
-                                <img id="imgShowed"></img>
-                                </IconButton>
+                            <IconButton onClick={this.clickInsertPhoto}><InsertPhoto />
+                                <input id="chooseFileModal" type="file" accept="image/*" onChange={this.onFileSelected}/>
+                                { /*TODO: DELETE THIS LINE IMG, ONLY FOR TEST*/}
+                                <img id="imgShowed" alt=""></img>
+                            </IconButton>
                         </ToolbarGroup>
                         <ToolbarGroup>
                             <ToolbarTitle text={this.state.title}/>
@@ -277,7 +282,7 @@ export default class EditorComponent extends Component {
                     <div id="accessibleDocument" className="document-edit"
                          contentEditable={true} dangerouslySetInnerHTML={this.state.content}
                          onClick={this.clickHandler}/>
-                    <Snackbar message={"Error : "+ this.state.errorMessage} open={this.state.errorSelected}/>
+                    <Snackbar message={"Error : " + this.state.errorMessage} open={this.state.errorSelected}/>
                 </Paper>
             </div>
         );
